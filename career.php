82fd8aeca9da57b0b978efbdb41074ed642fc028
Sneha <66251186+snehahello20@users.noreply.github.com>
2020-06-01 21:42:27 +1000

    Add files via upload

---
+++ career.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Salarydost - friend For fund</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="Easy loan,easy salary,salary,fast money,low interest,instant loan,jaldi money,jaldi loan,short term loan,home loan,
+        education loan,personal loan,business loan,flipkart,loan for salaried people,fatafat loan,low EMI,less EMI loan" name="keywords">
+  <meta content="Short term loan for Salaried people." name="description">
+
+  <!-- Favicons -->
+ <link href="img/favicon-16x16.png" type="image/png" sizes="16x16" rel="icon">
+  <link href="img/favicon-32x32.png" type="image/png" sizes="32x32" rel="icon">
+
+  <!-- Google Fonts -->
+   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,500,600,700,700i|Montserrat:300,400,500,600,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: NewBiz
+    Theme URL: https://bootstrapmade.com/newbiz-bootstrap-business-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body>
+
+   <!--==========================
+    Header
+  ============================-->
+   <?php
+    include_once('include/header.php');
+    ?>
+    
+     <!--==========================
+    Header end
+  ============================-->
+    <!--==========================
+      Career Section
+    ============================-->
+    
+<!--  --------------------------  Career Form ------------------------------------------------------------------->
+    <section id="career" style="margin-top:25px;">
+    <div class="container">
+        <div class="col-md-12 text-center" style="margin-bottom:70px;"  >
+        
+        <h2 style="color: #7e0343;font-weight:700;font-size:2.5em;margin-top:25px;">We are hiring for the following positions.</h2>
+     </div> 
+        <div class="row about-extra">
+            <div class="col-lg-5">
+        <img src="img/wearehiring.png" alt="" class="img-fluid">
+            </div>
+    <div class="col-lg-7 ">
+    <p style="text-align:justify;">Current openings: There are current openings as of listed below. At Salary Dost , we look out for individuals who are motivated and smart, and who want to be a part of this ever growing and happening industry. If you think you have got that fire in you to prove your mettle, then write to us at
+        <b>sheetal.kerekar@salarydost.com</b>
+        <br>  P.S. Enthusiasm at work is much needed and rewarded at Salary Dost .</p>
+<ul>
+
+    <li><i class="fa fa-book" style="font-size:26px;color:#7e0343"></i> <b>   Customer Care Executive-Thane and Mumbai </b><a href="careerapply.php?p=Customer Care" style="color:red" >(Apply)</a></li><br>
+    <li><i class="fa fa-book" style="font-size:26px;color:#7e0343"></i> <b>   Credit Underwriter - Thane and Mumbai</b><a href="careerapply.php?p=Credit Underwriter" style="color:red" > (Apply)</a></li><br>
+    <li><i class="fa fa-book" style="font-size:26px;color:#7e0343"></i><b>    Collection Executive - Thane and Mumbai</b><a href="careerapply.php?p=Collection Executive" style="color:red" > (Apply)</a></li><br>
+    <li><i class="fa fa-book" style="font-size:26px;color:#7e0343"></i><b>    Senior Software Developer(Cake PHP) - Thane and Mumbai </b><a href="careerapply.php?p=Senior Software Developer" style="color:red" > (Apply)</a></li><br>
+    <li><i class="fa fa-book" style="font-size:26px;color:#7e0343"></i><b>     Tele Caller - Thane and Mumbai</b><a href="careerapply.php?p= Tele Caller" style="color:red" > (Apply)</a></li><br>
+</ul>
+    </div>
+        </div>
+    </div>
+    </section>
+
+  <!--==========================
+    Footer
+  ============================-->
+   <?php
+    include_once('include/footer.php');
+    ?>
+    
+     <!--==========================
+    Footer end
+  ============================-->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/mobile-nav/mobile-nav.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script><!--
+-->  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+<!--  <script src="contactform/contactform.js"></script>-->
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
