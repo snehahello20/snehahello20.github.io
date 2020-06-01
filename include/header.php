742011a8bf0e55790326b035b084f131f0cbaa43
Sneha <66251186+snehahello20@users.noreply.github.com>
2020-06-01 21:41:18 +1000

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,55 @@
+<!--==========================
+  Header
+  ============================-->
+ <header id="header" >
+    <div class="container">
+
+      <div class="logo float-left">
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <a href="#intro" class="scrollto"><img src="img/logo.jpg" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav class="main-nav float-right d-none d-lg-block" >
+        <ul>
+          <li class="active"><a href="Salarydost.php">Home</a></li>
+          <li class="drop-down"><a href="#">About</a>
+              <ul>
+               <li><a href="about.php" >SalaryDost</a></li>
+               <li><a href="team.php">Team</a></li>
+              </ul>
+          </li>
+          <li class="drop-down"><a href="#">Community</a>
+              <ul>
+               <li><a href="docs.php">Documents Required</a></li>
+               <li><a href="faq.php">FAQ</a></li>
+               <li><a href="terms.php">Terms and Conditions</a></li>
+              </ul>
+          </li>
+          <li><a href="howtoapply.php">How to Apply?</a></li>
+          <li><a href="partner.php">Be our Partner</a></li>
+          <li><a href="http://blog.salarydost.com/">Blog</a></li>
+          <li><a href="contactus.php">Contact Us</a></li>
+         
+<!--          <li class="drop-down"><a href="">Drop Down</a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="drop-down"><a href="#">Drop Down 2</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">Drop Down 5</a></li>
+            </ul>
+          </li>-->
+<!--          <li><a href="#contact">Contact Us</a></li>-->
+        </ul>
+      </nav><!-- .main-nav -->
+      
+    </div>
+  </header><!-- #header -->
